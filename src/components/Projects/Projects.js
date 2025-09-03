3f05716be738c7332ed1a00e5094b804db115b4f
arkaghosh566 <arkaghosh566@gmail.com>
2025-09-03 19:43:00 +0530

    Uploaded project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import ETS from "../../Assets/Projects/ETS.png";
+import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import GAIT from "../../Assets/Projects/GAIT.png";
 import ETS from "../../Assets/Projects/ETS.png";
